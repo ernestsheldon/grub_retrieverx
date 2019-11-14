63df46704e9b1a8b2e9187cead067b3289067f61
ernestsheldon <ernestsheldon64@gmail.com>
2019-11-14 13:50:54 -0500

    first commit for grub retriever

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:grub_retriever/models/user.dart';
+import 'package:grub_retriever/screens/authenticate/authenticate.dart';
+import 'package:grub_retriever/screens/home/home.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
+    // return either the Home or Authenticate widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+  }
+}
